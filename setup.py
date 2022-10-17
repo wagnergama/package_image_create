e4a7ab284c7de6eba49b09e41ca767a6d982bccc
wagnergama <wagner@idealinformatica.info>
2022-10-17 15:52:33 -0300

    upgrade setup.py

--- setup.py
+++ setup.py
@@ -5,17 +5,19 @@ with open("README.md", "r") as f:
 
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
+    
+print(requirements)
 
 setup(
         name="image_create",
-        version="0.0.1",
+        version="0.0.2",
         author="wagner_gama",
         author_email="wagner@dealinformatica.info",
         description="pacote para criação de imagens",
         long_description=page_description,
         long_description_content_type="text/markdown",
         url="https://github.com/wagnergama/package_image_create",
-        packages=find_packages(),
+        packages = find_packages,
         install_requires=requirements,
         python_requires='>=3.8',
     )
