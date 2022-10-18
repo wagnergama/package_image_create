705a2ec4b53009a26360507c3a6c67b662a6e176
wagnergama <wagner@idealinformatica.info>
2022-10-18 10:29:54 -0300

    Reformulação do setup.py
    Removido algumas opções que estavam causando erro na distribuição

--- setup.py
+++ setup.py
@@ -1,23 +1,13 @@
-from setuptools import setup, find_packages
-
-with open("README.md", "r") as f:
-    page_description = f.read()
-
-with open("requirements.txt") as f:
-    requirements = f.read().splitlines()
-    
-print(requirements)
+from setuptools import setup
 
 setup(
-        name="image_create",
-        version="0.0.2",
-        author="wagner_gama",
-        author_email="wagner@dealinformatica.info",
-        description="pacote para criação de imagens",
-        long_description=page_description,
-        long_description_content_type="text/markdown",
-        url="https://github.com/wagnergama/package_image_create",
-        packages = find_packages,
-        install_requires=requirements,
-        python_requires='>=3.8',
-    )
+    name='image_create',
+    version='0.0.3',
+    author="wagner_gama",
+    author_email="wagner@dealinformatica.info",
+    description="pacote para criação de imagens",
+    install_requires=[
+        'Pillow==9.2.0',
+        'importlib-metadata; python_version == "3.10"',
+    ],
+)
